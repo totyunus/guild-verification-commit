8419896901d7430c8410b2409feb677455023886
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:51:50 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:51:26.281Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-12T14:51:52.072Z
+Commit: Add multi-language support
