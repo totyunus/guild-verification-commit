f37007c1491eeb31c24ef143b7a68fb58043a351
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:52:26 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:52:19.864Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-12T14:52:27.655Z
+Commit: Patch API endpoint error handling
