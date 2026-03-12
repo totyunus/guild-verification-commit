c89ff034fb78953827923e49842dc1c536d29ea0
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:53:05 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:52:52.120Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T14:53:06.941Z
+Commit: Resolve null pointer exception in user service
