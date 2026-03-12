e04eead07f810ed8b82bb653a3901559cdab9489
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:52:14 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:51:58.256Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-12T14:52:15.433Z
+Commit: Add multi-language support
