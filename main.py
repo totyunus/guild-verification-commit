403bdd2b75a96b3472982388cdf2c4467a8cb211
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:51:43 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:51:42.357Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T14:51:44.837Z
+Commit: Fix memory leak in event handlers
