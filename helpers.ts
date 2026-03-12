c8e420c42fa152f97968364a9efa8cacdcaa7cee
totyunus <166503078+totyunus@users.noreply.github.com>
2026-03-12 17:52:50 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:52:08.851Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T14:52:52.120Z
+Commit: Add export functionality for reports
